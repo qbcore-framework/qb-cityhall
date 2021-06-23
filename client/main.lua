ad9a2627ff467c6d7d2755c410fe94eb866d7799
viruspc2002 <79900651+viruspc2002@users.noreply.github.com>
2021-06-23 12:29:31 +0530

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,7 @@
 QBCore = nil
 
 Citizen.CreateThread(function()
-    while QBCore == nil then
+    while QBCore == nil do
        TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
        Citizen.Wait(200)
     end
