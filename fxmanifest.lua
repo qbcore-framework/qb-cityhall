c332a55abc61696c7c8adbb5b5255be7ca66585d
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-10-07 23:13:31 +0200

    feat(manifest): Add globbing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-CityHall'
+description 'Cityhall for QBCore'
 version '1.0.0'
 
 ui_page 'html/index.html'
 
 shared_script 'config.lua'
 
-server_script 'server/main.lua'
-client_script 'client/main.lua'
+server_script 'server/*.lua'
+client_script 'client/*.lua'
 
 files {
     'html/*.js',
