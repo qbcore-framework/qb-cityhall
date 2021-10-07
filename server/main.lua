4eca0e4aa6dc18a390e4089475e1a06d19f851be
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-10-07 22:25:35 +0200

    refactor!: Big change
    - Added more security
    - Changed Driving instructor getter
    - Added back drivinglicense command with more security and a check

--- server/main.lua
+++ server/main.lua
@@ -1,35 +1,33 @@
-local DrivingSchools = {
-    
-}
+local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterServerEvent('qb-cityhall:server:requestId')
 AddEventHandler('qb-cityhall:server:requestId', function(identityData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local info = {}
-    if identityData.item == "id_card" then
-        info.citizenid = Player.PlayerData.citizenid
-        info.firstname = Player.PlayerData.charinfo.firstname
-        info.lastname = Player.PlayerData.charinfo.lastname
-        info.birthdate = Player.PlayerData.charinfo.birthdate
-        info.gender = Player.PlayerData.charinfo.gender
-        info.nationality = Player.PlayerData.charinfo.nationality
-    elseif identityData.item == "driver_license" then
-        info.firstname = Player.PlayerData.charinfo.firstname
-        info.lastname = Player.PlayerData.charinfo.lastname
-        info.birthdate = Player.PlayerData.charinfo.birthdate
-        info.type = "Class C Driver License"
-    elseif identityData.item == "weaponlicense" then
-        info.firstname = Player.PlayerData.charinfo.firstname
-        info.lastname = Player.PlayerData.charinfo.lastname
-        info.birthdate = Player.PlayerData.charinfo.birthdate
+    if Player then
+        if identityData.item == "id_card" then
+            info.citizenid = Player.PlayerData.citizenid
+            info.firstname = Player.PlayerData.charinfo.firstname
+            info.lastname = Player.PlayerData.charinfo.lastname
+            info.birthdate = Player.PlayerData.charinfo.birthdate
+            info.gender = Player.PlayerData.charinfo.gender
+            info.nationality = Player.PlayerData.charinfo.nationality
+        elseif identityData.item == "driver_license" then
+            info.firstname = Player.PlayerData.charinfo.firstname
+            info.lastname = Player.PlayerData.charinfo.lastname
+            info.birthdate = Player.PlayerData.charinfo.birthdate
+            info.type = "Class C Driver License"
+        elseif identityData.item == "weaponlicense" then
+            info.firstname = Player.PlayerData.charinfo.firstname
+            info.lastname = Player.PlayerData.charinfo.lastname
+            info.birthdate = Player.PlayerData.charinfo.birthdate
+        end
+
+        Player.Functions.AddItem(identityData.item, 1, false, info)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[identityData.item], 'add')
     end
-
-    Player.Functions.AddItem(identityData.item, 1, nil, info)
-
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[identityData.item], 'add')
 end)
-
 
 RegisterServerEvent('qb-cityhall:server:getIDs')
 AddEventHandler('qb-cityhall:server:getIDs', function()
@@ -37,86 +35,86 @@ AddEventHandler('qb-cityhall:server:getIDs', function()
     GiveStarterItems(src)
 end)
 
-
 RegisterServerEvent('qb-cityhall:server:sendDriverTest')
 AddEventHandler('qb-cityhall:server:sendDriverTest', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    for k, v in pairs(DrivingSchools) do 
-        local SchoolPlayer = QBCore.Functions.GetPlayerByCitizenId(v)
-        if SchoolPlayer ~= nil then 
-            TriggerClientEvent("qb-cityhall:client:sendDriverEmail", SchoolPlayer.PlayerData.source, Player.PlayerData.charinfo)
-        else
-            local mailData = {
-                sender = "Township",
-                subject = "Driving lessons request",
-                message = "Hello,<br /><br />We have just received a message that someone wants to take driving lessons.<br />If you are willing to teach, please contact us:<br />Naam: <strong>".. Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. "<br />Telephone number: <strong>"..Player.PlayerData.charinfo.phone.."</strong><br/><br/>Kind regards,<br />City of Los Santos",
-                button = {}
-            }
-            TriggerEvent("qb-phone:server:sendNewEventMail", v, mailData)
+    if Player then
+        for k, v in pairs(Config.DrivingInstructors)
+            local SchoolPlayer = QBCore.Functions.GetPlayerByCitizenId(k)
+            if SchoolPlayer then 
+                TriggerClientEvent("qb-cityhall:client:sendDriverEmail", SchoolPlayer.PlayerData.source, Player.PlayerData.charinfo)
+            else
+                local mailData = {
+                    sender = "Township",
+                    subject = "Driving lessons request",
+                    message = "Hello,<br /><br />We have just received a message that someone wants to take driving lessons.<br />If you are willing to teach, please contact us:<br />Name: <strong>".. Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. "<br />Phone number: <strong>"..Player.PlayerData.charinfo.phone.."</strong><br/><br/>Kind regards,<br />City of Los Santos",
+                    button = {}
+                }
+                TriggerEvent("qb-phone:server:sendNewEventMail", k, mailData)
+            end
         end
+        TriggerClientEvent('QBCore:Notify', src, 'An email has been sent to driving schools, and you will be contacted automatically', "success", 5000)
     end
-    TriggerClientEvent('QBCore:Notify', src, 'An email has been sent to driving schools, and you will be contacted automatically', "success", 5000)
 end)
 
 RegisterServerEvent('qb-cityhall:server:ApplyJob')
 AddEventHandler('qb-cityhall:server:ApplyJob', function(job)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local JobInfo = QBCore.Shared.Jobs[job]
-
-    Player.Functions.SetJob(job, 0)
-
-    TriggerClientEvent('QBCore:Notify', src, 'Congratulations with your new job! ('..JobInfo.label..')')
+    if Player then
+        Player.Functions.SetJob(job, 0)
+        TriggerClientEvent('QBCore:Notify', src, 'Congratulations with your new job! ('..QBCore.Shared.Jobs[job].label..')')
+    end
 end)
 
-
--- QBCore.Commands.Add("drivinglicense", "Give a driver's license to someone", {{"id", "ID of a person"}}, true, function(source, args)
---     local Player = QBCore.Functions.GetPlayer(source)
-
---         local SearchedPlayer = QBCore.Functions.GetPlayer(tonumber(args[1]))
---         if SearchedPlayer ~= nil then
---             local driverLicense = SearchedPlayer.PlayerData.metadata["licences"]["driver"]
---             if not driverLicense then
---                 local licenses = {
---                     ["driver"] = true,
---                     ["business"] = SearchedPlayer.PlayerData.metadata["licences"]["business"]
---                 }
---                 SearchedPlayer.Functions.SetMetaData("licences", licenses)
---                 TriggerClientEvent('QBCore:Notify', SearchedPlayer.PlayerData.source, "You have passed! Pick up your driver's license at the town hall", "success", 5000)
---             else
---                 TriggerClientEvent('QBCore:Notify', src, "Can't give driver's license ..", "error")
---             end
---         end
-
--- end)
+QBCore.Commands.Add("drivinglicense", "Give a drivers license to someone", {{"id", "ID of a person"}}, true, function(source, args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player then
+        if Config.DrivingInstructors[Player.PlayerData.citizenid] then
+            local SearchedPlayer = QBCore.Functions.GetPlayer(tonumber(args[1]))
+            if SearchedPlayer then
+                if not SearchedPlayer.PlayerData.metadata["licences"]["driver"] then
+                    SearchedPlayer.PlayerData.metadata['licences']['driver'] = true
+                    SearchedPlayer.Functions.SetMetaData("licences", SearchedPlayer.PlayerData.metadata['licences'])
+                    TriggerClientEvent('QBCore:Notify', SearchedPlayer.PlayerData.source, "You have passed! Pick up your driver's license at the town hall", "success", 5000)
+                else
+                    TriggerClientEvent('QBCore:Notify', src, "Can't give driver's license ..", "error")
+                end
+            end
+        else
+            TriggerClientEvent('QBCore:Notify', src, "You don't have access to this command.", "error")
+        end
+    end
+end)
 
 function GiveStarterItems(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-
-    for k, v in pairs(QBCore.Shared.StarterItems) do
-        local info = {}
-        if v.item == "id_card" then
-            info.citizenid = Player.PlayerData.citizenid
-            info.firstname = Player.PlayerData.charinfo.firstname
-            info.lastname = Player.PlayerData.charinfo.lastname
-            info.birthdate = Player.PlayerData.charinfo.birthdate
-            info.gender = Player.PlayerData.charinfo.gender
-            info.nationality = Player.PlayerData.charinfo.nationality
-        elseif v.item == "driver_license" then
-            info.firstname = Player.PlayerData.charinfo.firstname
-            info.lastname = Player.PlayerData.charinfo.lastname
-            info.birthdate = Player.PlayerData.charinfo.birthdate
-            info.type = "Class C Driver License"
+    if Player then
+        for k, v in pairs(QBCore.Shared.StarterItems) do
+            local info = {}
+            if v.item == "id_card" then
+                info.citizenid = Player.PlayerData.citizenid
+                info.firstname = Player.PlayerData.charinfo.firstname
+                info.lastname = Player.PlayerData.charinfo.lastname
+                info.birthdate = Player.PlayerData.charinfo.birthdate
+                info.gender = Player.PlayerData.charinfo.gender
+                info.nationality = Player.PlayerData.charinfo.nationality
+            elseif v.item == "driver_license" then
+                info.firstname = Player.PlayerData.charinfo.firstname
+                info.lastname = Player.PlayerData.charinfo.lastname
+                info.birthdate = Player.PlayerData.charinfo.birthdate
+                info.type = "Class C Driver License"
+            end
+            Player.Functions.AddItem(v.item, 1, false, info)
         end
-        Player.Functions.AddItem(v.item, 1, false, info)
     end
 end
 
 function IsWhitelistedSchool(citizenid)
     local retval = false
-    for k, v in pairs(DrivingSchools) do 
+    for k, v in pairs(Config.DrivingIntructors) do 
         if v == citizenid then
             retval = true
         end
