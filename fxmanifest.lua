5a32c5476f9e65297645a969c9635aa67d2af2eb
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-03-05 21:03:12 +0100

    refactor!: cityhall update
    Read https://github.com/qbcore-framework/qb-cityhall/issues/41

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,18 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-CityHall'
-version '1.0.0'
+version '2.0.0'
 
 ui_page 'html/index.html'
 
 shared_script 'config.lua'
+server_script 'server/main.lua'
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    'client/main.lua'
 }
-
-client_script 'client/main.lua'
 
 files {
     'html/*.js',
