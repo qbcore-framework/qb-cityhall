d368c00f877498e3886d03f7f5e810b8a36307b7
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-04-03 21:19:30 -0500

    Remove unused config options

--- config.lua
+++ config.lua
@@ -5,11 +5,3 @@ Config.CompanyPrice = 25000
 Config.Cityhall = {
     coords = vector3(-265.0, -963.6, 31.2)
 }
-
-Config.DriverTest = {
-    coords = vector3(-549.86, -191.75, 38.22)
-}
-
-Config.DrivingSchool = {
-    coords = vector3(232.5, 368.7, 105.94)
-}
