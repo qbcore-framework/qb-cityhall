8bef2bbf4ee48abe951f5b0cee53fba36b26c900
buddizer <lucasg@live.com>
2022-05-03 02:23:36 -0600

    Create es.lua
    Adding spanish translation

---
+++ locales/es.lua
@@ -0,0 +1,28 @@
+local Translations = {
+    error = {
+        not_in_range = 'Estás muy lejos de la municipalidad'
+    },
+    success = {
+        recived_license = 'Has recibido tu %{value} por $50'
+    },
+    info = {
+        bilp_text = 'Servicios de la ciudad',
+        city_services_menu = '~g~E~w~ - Menu de servicios de la ciudad',
+        id_card = 'Identificación',
+        driver_license = 'Licencia de conducir',
+        weaponlicense = 'Licencia de armas',
+        new_job = '¡Felicidades con tu nuevo trabajo! (%{job})'
+    },
+    email = {
+        mr = 'Sr',
+        mrs = 'Sra',
+        sender = 'Municipio',
+        subject = 'Solicitud de clases de manejo',
+        message = 'Hola %{gender} %{lastname}<br /><br />Acabamos de recibir un mensaje de alguien que quiere tomar clases de manejo<br />Si estás dispuesto a enseñar, por favor contáctanos:<br />Nombre: <strong>%{firstname} %{lastname}</strong><br />Número de teléfono: <strong>%{phone}</strong><br/><br/>Saludos,<br />Municipio de Los Santos'
+    }
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
