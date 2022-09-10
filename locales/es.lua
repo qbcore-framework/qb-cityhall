9d55882ea5565ab4040060cadad36d54c68143c2
buddizer <none>
2022-09-10 02:11:12 -0600

    auto locale switching system

--- locales/es.lua
+++ locales/es.lua
@@ -1,28 +1,31 @@
 local Translations = {
     error = {
-        not_in_range = 'Muy lejos del ayuntamiento'
+        not_in_range = 'Muy lejos de la municipalidad'
     },
     success = {
         recived_license = 'Has recibido tu %{value} por $50'
     },
     info = {
-        bilp_text = 'Servicios Civiles',
-        city_services_menu = '~g~E~w~ - Servicios Civiles',
+        bilp_text = 'Servicios municipales',
+        city_services_menu = '[E] - Servicios municipales',
         id_card = 'DNI',
-        driver_license = 'Licencia de Conducir',
-        weaponlicense = 'Licencia de Armas',
-        new_job = 'Felicidades en tu nuevo trabajo! (%{job})'
+        driver_license = 'Licencia de conducir',
+        weaponlicense = 'Licencia para portacion de armas',
+        new_job = 'Felicidades por tu nuevo trabajo! (%{job})'
     },
     email = {
         mr = 'Sr',
         mrs = 'Sra',
-        sender = 'Ayuntamiento',
+        sender = 'Municipalidad',
         subject = 'Solicitud de clases de conducir',
         message = 'Hola %{gender} %{lastname}<br /><br />Acabamos de recibir un mensaje de que deseas clases de conducir<br />Si deseas enseñar, contactanos:<br />Nombre: <strong>%{firstname} %{lastname}</strong><br />Numero de teléfono: <strong>%{phone}</strong><br/><br/>Saludos,<br />Ayuntamiento Los Santos'
     }
 }
 
-Lang = Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'es' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
