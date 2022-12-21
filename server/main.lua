a358c512227572daba6285d9116f28cf81aa9647
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-12-21 13:01:20 +0100

    Change event to export

--- server/main.lua
+++ server/main.lua
@@ -100,7 +100,7 @@ RegisterNetEvent('qb-cityhall:server:sendDriverTest', function(instructors)
                 message = "Hello,<br><br>We have just received a message that someone wants to take driving lessons.<br>If you are willing to teach, please contact them:<br>Name: <strong>".. Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. "<br />Phone Number: <strong>"..Player.PlayerData.charinfo.phone.."</strong><br><br>Kind regards,<br>Township Los Santos",
                 button = {}
             }
-            TriggerEvent("qb-phone:server:sendNewMailToOffline", citizenid, mailData)
+            exports["qb-phone"]:sendNewMailToOffline(citizenid, mailData)
         end
     end
     TriggerClientEvent('QBCore:Notify', src, "An email has been sent to driving schools, and you will be contacted automatically", "success", 5000)
