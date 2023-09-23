9eda5cb946b8de5c87b1925f95efe24feb5adc52
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2023-09-23 15:35:39 -0500

    Workflow

--- client/main.lua
+++ client/main.lua
@@ -73,7 +73,7 @@ local function createBlip(options)
     SetBlipColour(blip, options.colour or 1)
     SetBlipAsShortRange(blip, options.shortRange or false)
     BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(options.title or 'No Title Given')
+    AddTextComponentSubstringPlayerName(options.title or 'No Title Given')
     EndTextCommandSetBlipName(blip)
     return blip
 end
