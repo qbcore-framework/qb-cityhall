b6893c49eec3a0b5a043c7de3c8a4ab41137ad78
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-01-15 00:31:28 -0600

    Money reasons

--- server/main.lua
+++ server/main.lua
@@ -51,7 +51,7 @@ RegisterNetEvent('qb-cityhall:server:requestId', function(item, hall)
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
     local itemInfo = Config.Cityhalls[hall].licenses[item]
-    if not Player.Functions.RemoveMoney('cash', itemInfo.cost) then return TriggerClientEvent('QBCore:Notify', src, ('You don\'t have enough money on you, you need %s cash'):format(itemInfo.cost), 'error') end
+    if not Player.Functions.RemoveMoney('cash', itemInfo.cost, 'cityhall id') then return TriggerClientEvent('QBCore:Notify', src, ('You don\'t have enough money on you, you need %s cash'):format(itemInfo.cost), 'error') end
     local info = {}
     if item == 'id_card' then
         info.citizenid = Player.PlayerData.citizenid
